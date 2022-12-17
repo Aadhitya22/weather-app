b91d65902654cc8a02461d09d0ec8cdfaa3b0b51
Aadhitya Kumar <aadhitya.k22@gmail.com>
2022-12-17 21:25:50 +0530

    Added stlying

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const bodyParser = require("body-parser");
+const exp = require("constants");
 const express = require("express");
 const https = require("https");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -25,15 +27,24 @@ app.post("/", function (req, res) {
       const description = weatherData.weather[0].description;
       const imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
       res.set("Content-Type", "text/html");
-      res.write("<p>The weather is currently " + description + "</p>");
       res.write(
-        "<h1>The temperature in " +
+        "<body style='background-color: rgb(182, 222, 235);text-align: center; margin:3rem;'"
+      );
+      res.write("<p>Hello!</p>");
+      res.write(
+        "<p style='margin: 1rem;'>The weather is currently " +
+          description +
+          "</p>"
+      );
+      res.write("<img src=" + imgUrl + ">");
+      res.write(
+        "<h1 style='margin: 2rem;'>The temperature in " +
           city +
           " is " +
           temp +
           " degrees Celsius</h1>"
       );
-      res.write("<img src=" + imgUrl + ">");
+
       res.send();
     });
   });
